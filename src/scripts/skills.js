8b5e07cde6f48ef13a088c482f96478988375205
Mihail Yarantsev <mihaalich2@mail.ru>
2019-07-06 10:06:07 +0300

    migration skill in vue

--- src/scripts/skills.js
+++ src/scripts/skills.js
@@ -1,1 +1,46 @@
-console.log('this is skills module');
+import Vue from 'vue';
+import skills from './../pug/components/skills/skills';
+
+const skill = {
+  template: "#skill",
+  props: {
+    name: String,
+    percent: Number
+  }
+};
+
+const skillList = {
+  template: "#skill-list",
+  props: {
+    skills: Object
+  },
+  components: {
+    skill
+  }
+};
+
+const skillsGroup = {
+  template: "#skills-group",
+  props: {
+    skillGroupList: Object
+  },
+  components: {
+    skillList
+  }
+};
+
+new Vue({
+  el: "#skills-component",
+  template: '#skills-template',
+  data() {
+    return {
+      skills: []
+    };
+  },
+  components: {
+    skillsGroup
+  },
+  created() {
+    this.skills = skills;
+  }
+});
