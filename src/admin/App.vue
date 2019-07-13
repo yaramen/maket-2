80dab38fb745e19f174c513e6ca283bdcee1f130
Mihail Yarantsev <mihaalich2@mail.ru>
2019-07-14 00:47:08 +0300

    add admin makeup

--- src/admin/App.vue
+++ src/admin/App.vue
@@ -1,3 +1,14 @@
 <template lang="pug">
-  h1 Welcome to the Vue App
+    div
+        include components/section/header.pug
+        main
+            sectoin.content
+                .content__header
+                    .content__title Блок "Обо мне"
+                    .content__add-group
+                        button.btn.-group Добавить группу
+                .content__body
+                    include components/section/skills-controller.pug
+                    include components/section/works-controller.pug
+                    include components/section/works-controller.pug
 </template>
