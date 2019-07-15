1775335c769e6f527bab7223f9c521ae98533a30
Mihail Yarantsev <mihaalich2@mail.ru>
2019-07-15 21:31:58 +0300

    add login page

--- src/admin/App.vue
+++ src/admin/App.vue
@@ -1,14 +1,21 @@
 <template lang="pug">
     div
+        include components/section/auth.pug
         include components/section/header.pug
         main
-            sectoin.content
+            section.content
                 .content__header
                     .content__title Блок "Обо мне"
                     .content__add-group
-                        button.btn.-group Добавить группу
+                        button.content__add
+                            span.content__add-plus +
+                            span.content__add-text Добавить группу
                 .content__body
                     include components/section/skills-controller.pug
                     include components/section/works-controller.pug
-                    include components/section/works-controller.pug
+                    include components/section/review-controller.pug
 </template>
+
+<style lang="postcss">
+    @import "style/main.pcss";
+</style>
