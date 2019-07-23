41d05584026f51c8e6f4d523dafbb5775f65a6b5
Mihail Yarantsev <mihaalich2@mail.ru>
2019-07-23 22:20:27 +0300

    add tests

---
+++ wdio.conf.js
@@ -0,0 +1,23 @@
+const shared = {
+  host: 'localhost',
+  port: 4444,
+  path: '/wd/hub',
+  coloredLogs: true,
+  screenshotPath: './errorShots/',
+  waitforTimeout: 10000,
+  connectionRetryTimeout: 90000,
+  connectionRetryCount: 3,
+  maxInstances: 1
+};
+
+
+exports.chrome = {
+  ...shared,
+  desiredCapabilities: {
+    browserName: 'chrome',
+    chromeOptions: {
+      args: ["disable-gpu", 'no-sandbox', 'disable-setuid-sandbox', 'incognito', 'disable-dev-shm-usage']
+    }
+  }
+};
+
